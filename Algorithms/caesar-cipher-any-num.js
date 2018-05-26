03101127cb3dd7adadc4b900718b8ebcef809c62
mwong770 <mwong770@gmail.com>
2018-05-25 22:21:22 -0400

    removes some algorithms

--- Algorithms/caesar-cipher-any-num.js
+++ Algorithms/caesar-cipher-any-num.js
@@ -1,32 +1,35 @@
 //replace each letter of string with the letter of the alphabet that is a certain number away
 //num can be positive or negative
 
-function caesarCipher(str,num) {
+function cesar(orgString, num) {
   //if pass in num < or > +-26, this will give an # between 1 and 26
   num = num % 26;
-  var lowerCaseString = str.toLowerCase();
-  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  var newString = '';
-  
-  for (var i = 0; i < lowerCaseString.length; i++) {
-    var currentLetter = lowerCaseString[i];
-    if (currentLetter === ' ') {
-      newString += currentLetter;
-      //move on to next iteration
-      continue;
-    }
-    var currentIndex = alphabet.indexOf(currentLetter);
-    var newIndex = currentIndex + num;
-    if (newIndex > 25) newIndex = newIndex - 26;
-    if (newIndex < 0) newIndex = 26 + newIndex;
-    if (str[i] === str[i].toUpperCase()) {
-      newString += alphabet[newIndex].toUpperCase();
-    }
-    else newString += alphabet[newIndex];
-  };
-  
+  //if a negative number, will return a positive number
+  if (Math.sign(num) === -1 || Math.sign(num) === -0) {
+    num = -num;
+  }
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  lowerOrgString = orgString.toLowerCase();
+  var newString="";
+  for (var i=0; i< lowerOrgString.length; i++) {
+      if (lowerOrgString[i].match(/[a-z]/)) {
+            newIndex = alphabet.indexOf(lowerOrgString[i]) + num;
+            if (newIndex >= 26) {
+              newIndex -=26
+            }
+            newLetter = alphabet[newIndex];
+      } else {
+            newLetter = lowerOrgString[i];
+      }
+     if (orgString[i] === orgString[i].toUpperCase()) {
+        newString += newLetter.toUpperCase();
+      } else  {
+       newString += newLetter;
+      }
+  } //ends for loop
   return newString;
-}
+} //ends function
 
-caesarCipher('Big Car', -16);//returns 'Lsq Mkb'
-caesarCipher('zoo keeper', 2);//returns 'bqq mggrgt'
+cesar("Hello this is a string.", 13); ////"Uryyb guvf vf n fgevat."
+
+caesarCipher('zoo keeper', 2); //returns 'bqq mggrgt'
